478c9ce576f746fc8c997240adff095ce25d556f
soya Park <uyyu1734@gmail.com>
2020-08-08 00:41:48 +0900

    모의고사(7월초에 푼문제),8/12 스터디 예정 "rename the author"

--- src/programmers_모의고사.java
+++ src/programmers_모의고사.java
@@ -6,28 +6,43 @@ public class programmers_모의고사 {
     static boolean[] visited ;
     static int[] digits;
     public static void main(String[] args) {
-       String answers="";
-        int ans = solution(answers);
+        int[] answers={};
+        int[] ans = solution(answers);
         System.out.println(ans);
     }
-    public static int solution(String numbers) { // 1~7
-        int answer = 0;
-        visited = new boolean[numbers.length()];
-        digits = new int[numbers.length()];
-
-        for (int i = 0; i <numbers.length() ; i++) {
-            digits[i]=numbers.charAt(i)-'0';
+    public static int[] solution(int[] answers) {
+        int[] answer = {};
+        int[] one = {1,2,3,4,5} ;// 5
+        int[] two ={2,1,2,3,2,4,2,5}; //8
+        int[] three ={3,3,1,1,2,2,4,4,5,5} ;//10
+        int[] cnt = {0,0,0};
+        int idx = 0;
+        while(idx < answers.length){
+            if(one[idx%5] == answers[idx])
+                cnt[0]++;
+            if(two[idx%8] == answers[idx])
+                cnt[1]++;
+            if(three[idx%10] == answers[idx])
+                cnt[2]++;
+            idx++;
+        }
+        int winner = cnt[0]; //제일 많이 맞은 값
+        for(int i =0; i<cnt.length;i++){
+            if(winner < cnt[i]){
+                winner = cnt[i];
+            }
         }
 
-        comb(0,0);
-        //1 숫자 조합을 만든다.
-        //2 소수인지 체크
-
-
+        List<Integer> wins = new ArrayList<Integer>();
+        for(int i =0; i<cnt.length;i++){
+            if(winner == cnt[i]){
+                wins.add(i);
+            }
+        }
+        answer = new int[wins.size()];
+        for(int i=0; i<wins.size();i++){
+            answer[i]=wins.get(i)+1;
+        }
         return answer;
     }
-    public static void comb (int now, int before){
-
-    }
-
 }
