f385d8e28da48d6fba785649c189e445a941d249
soyalattee <uyyu173@naver.com>
2020-08-12 23:00:42 +0900

    프로그래머스-크레인인형뽑

---
+++ src/programmers_크레인인형뽑기.java
@@ -0,0 +1,36 @@
+import java.util.Queue;
+import java.util.Stack;
+
+public class programmers_크레인인형뽑기 {
+    public static void main(String[] args) {
+        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}}; //5~30
+        int[] moves = {1,5,3,5,1,2,1,4}; //0~100
+        System.out.println(solution(board,moves));
+    }
+
+
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> basket = new Stack<>();
+        int [] lastIdx = new int[board.length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j <board.length ; j++) {
+                System.out.print(board[j][i]);
+                if(board[j][i]!=0){
+                    lastIdx[i]=j;
+                    break;
+                }
+            }
+            System.out.println();
+        }
+        for (int i: lastIdx) {
+            System.out.print(i);
+        }
+
+        for (int i = 0; i < moves.length; i++) {
+
+        }
+        return answer;
+    }
+
+}
